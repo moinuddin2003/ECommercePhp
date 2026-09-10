e5ad9851c606ca1bae21dd703cc296b7bea52d1b
Muhammad Moinuddin <moinuddin010203@gmail.com>
2026-09-10 17:14:02 +0500

    first commit

---
+++ config/database.php
@@ -0,0 +1,11 @@
+<?php
+// Base domain URL
+// define('APP_URL', 'http://ecommerce.local');
+
+// Filesystem root path
+define('BASE_PATH', dirname(__DIR__));
+
+// Front assets URL
+// define('FRONT_ASSETS', APP_URL . '/assets/front');
+
+?>
